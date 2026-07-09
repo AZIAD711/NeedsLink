b6758f25cd0cd2dbdffd4d76c12f821806e9214d
zeyad <aziad711@gmail.com>
2026-07-09 12:22:55 +0300

    ✅  ADD NEW ITEM IN TOKEN : VERFIY TOKEN

--- backend/src/common/token/token.js
+++ backend/src/common/token/token.js
@@ -1,14 +1,18 @@
 import jwt from "jsonwebtoken";
 // GENERATE TOKEN 
 export const generateToken = ({
-    payload , 
+    payload,
     secretKey,
-    options ={
-        expiresIn : "1h",
-        notBefore : "0",
-        audience : "user",
-        issuer : "needslink",
+    options = {
+        expiresIn: "1h",
+        notBefore: "0",
+        audience: "user",
+        issuer: "NeedsLink",
     }
-})=>{
- return jwt.sign(payload , secretKey , options)   
+}) => {
+    return jwt.sign(payload, secretKey, options)
+}
+// VERIFY TOKEN 
+export const verifyToken = (token, secretKey) => {
+    return jwt.verify(token, secretKey)
 }
